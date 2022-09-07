a566c6dfe14aad5d51476c906ae418502e7433ab
santiagoparadelo <santiago@gifstudio.com.ar>
2022-09-07 03:51:58 +0000

    Cambios a la API de Flask

--- api.py
+++ api.py
@@ -6,7 +6,13 @@ app.config["DEBUG"] = True # Activo debug para mensajes en consola
 
 @app.route('/home', methods=['GET']) # Ruta que brinda los programas para la pagina HOME
 def home():
-    channels = parseXML('home.xml')
+    channels = parseXML('outputs/home.xml')
     return channels
+
+@app.route('/all', methods=['GET']) # Ruta que brinda los programas para la pagina HOME
+def home():
+    channels = parseXML('outputs/all.xml')
+    return channels
+
 
 app.run(host='0.0.0.0')
